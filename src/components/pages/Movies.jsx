df5022b4fcd0cbaf67c093f3a0602489235d24f0
Cam <cameronwatkins46@gmail.com>
2024-06-07 16:26:05 -0500

    movies page updated

--- src/components/pages/Movies.jsx
+++ src/components/pages/Movies.jsx
@@ -1,11 +1,15 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Nav from "../ui/Nav";
+import { useParams } from "react-router-dom";
 
 const API_URL = "http://www.omdbapi.com/?apikey=cca6a59";
 
 function Movies() {
   const [movies, setMovies] = useState([]);
+  const params = useParams()
+  const title = params.id
+  
 
   async function fetchMovies(title) {
     const { data } = await axios.get(`${API_URL}&s=${title}`);
@@ -13,14 +17,21 @@ function Movies() {
   }
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(title);
+  }, [title]);
   return (
     <>
     <Nav />
       <div className="movie__row">
         <div className="movie__wrapper">
           {movies.map((movie) => (
+            ? new Array(8).fill(0).map((_, index) => {
+              <div className="movie">
+                <div className="movie__img--skeleton">
+
+                </div>
+              </div>
+            })
             <div className="movie">
               <div className="movie__img" key={movie.id}>
                 <img src={`${movie.Poster}`} alt="poster" />
@@ -32,6 +43,7 @@ function Movies() {
               </div>
             </div>
           ))}
+          
         </div>
       </div>
     </>
