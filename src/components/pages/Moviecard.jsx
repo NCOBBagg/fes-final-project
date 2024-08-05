0f8c4d210752982acc4652d481cbc831304853ab
Cam <cameronwatkins46@gmail.com>
2024-08-05 12:25:06 -0500

    new commit

--- src/components/pages/Moviecard.jsx
+++ src/components/pages/Moviecard.jsx
@@ -9,46 +9,54 @@ function Moviecard() {
   const params = useParams();
   const imdbID = params.id;
   const [desc, setDesc] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   async function fecthDesc() {
     const { data } = await axios.get(`${API_URL}&i=${imdbID}`);
-    setDesc(data.Search)
+    setDesc(data.Search);
+    setLoading(false);
   }
 
   useEffect(() => {
     fecthDesc();
-  }, (""));
+  }, []);
 
   return (
     <>
       <Nav />
-      <div className="movie__img--wrapper">
-        <h1>${movie.Title}</h1>
-        <img src={`${movie.Poster}`} alt="" />
-      </div>
-      <div className="movie__info--wrapper" key={imdbID}>
-        <h3>
-          <span className="red">Released: </span>${movie.Released}
-        </h3>
-        <h3>
-          <span className="red">Actors: </span>${movie.Actors}
-        </h3>
-        <h3>
-          <span className="red">Genre: </span>${movie.Genre}
-        </h3>
-        <h3>
-          <span className="red">Director: </span>${movie.Director}
-        </h3>
-        <h3>
-          <span className="red">Writer: </span>${movie.Writer}
-        </h3>
-        <h3>
-          <span className="red">Language: </span>${movie.Language}
-        </h3>
-        <h3>
-          <span className="red">Plot: </span>${movie.Plot}
-        </h3>
-      </div>
+
+      {loading
+        ? new Array(1).fill(0).map((_, movie) => (
+            <div className="movie__img--wrapper">
+              <h1>${movie.Title}</h1>
+              <img src={`${movie.Poster}`} alt="" />
+            </div>
+          ))
+        : desc.map((movie) => (
+            <div className="movie__info--wrapper" key={imdbID}>
+              <h3>
+                <span className="red">Released: </span>${movie.Released}
+              </h3>
+              <h3>
+                <span className="red">Actors: </span>${movie.Actors}
+              </h3>
+              <h3>
+                <span className="red">Genre: </span>${movie.Genre}
+              </h3>
+              <h3>
+                <span className="red">Director: </span>${movie.Director}
+              </h3>
+              <h3>
+                <span className="red">Writer: </span>${movie.Writer}
+              </h3>
+              <h3>
+                <span className="red">Language: </span>${movie.Language}
+              </h3>
+              <h3>
+                <span className="red">Plot: </span>${movie.Plot}
+              </h3>
+            </div>
+          ))}
     </>
   );
 }
--- src/components/pages/Movies.jsx
+++ src/components/pages/Movies.jsx
@@ -16,7 +16,7 @@ function Movies() {
 
     Navigate(`/moviecard/${movies}`);
 
-    // fetchMovies(movies)
+    fetchMovies(movies)
   }
 
   async function fetchMovies(title) {
