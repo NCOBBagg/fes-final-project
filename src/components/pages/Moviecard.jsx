7b494e7815f3e18fa8e474db94ae49428d2584e9
Cam <cameronwatkins46@gmail.com>
2024-08-09 14:31:44 -0500

    mapping error

--- src/components/pages/Moviecard.jsx
+++ src/components/pages/Moviecard.jsx
@@ -24,39 +24,37 @@ function Moviecard() {
   return (
     <>
       <Nav />
-
-      {loading
-        ? new Array(1).fill(0).map((_, movie) => (
-            <div className="movie__img--wrapper">
-              <h1>${movie.Title}</h1>
-              <img src={`${movie.Poster}`} alt="" />
-            </div>
-          ))
-        : desc.map((movie) => (
-            <div className="movie__info--wrapper" key={movie.id}>
-              <h3>
-                <span className="red">Released: </span>${movie.Released}
-              </h3>
-              <h3>
-                <span className="red">Actors: </span>${movie.Actors}
-              </h3>
-              <h3>
-                <span className="red">Genre: </span>${movie.Genre}
-              </h3>
-              <h3>
-                <span className="red">Director: </span>${movie.Director}
-              </h3>
-              <h3>
-                <span className="red">Writer: </span>${movie.Writer}
-              </h3>
-              <h3>
-                <span className="red">Language: </span>${movie.Language}
-              </h3>
-              <h3>
-                <span className="red">Plot: </span>${movie.Plot}
-              </h3>
-            </div>
-          ))}
+      {desc.map((movie) => (
+        <>
+          <div className="movie__img--wrapper">
+            <h1>${movie.Title}</h1>
+            <img src={`${movie.Poster}`} alt="" />
+          </div>
+          <div className="movie__info--wrapper" key={movie.id}>
+            <h3>
+              <span className="red">Released: </span>${movie.Released}
+            </h3>
+            <h3>
+              <span className="red">Actors: </span>${movie.Actors}
+            </h3>
+            <h3>
+              <span className="red">Genre: </span>${movie.Genre}
+            </h3>
+            <h3>
+              <span className="red">Director: </span>${movie.Director}
+            </h3>
+            <h3>
+              <span className="red">Writer: </span>${movie.Writer}
+            </h3>
+            <h3>
+              <span className="red">Language: </span>${movie.Language}
+            </h3>
+            <h3>
+              <span className="red">Plot: </span>${movie.Plot}
+            </h3>
+          </div>
+        </>
+      ))}
     </>
   );
 }
